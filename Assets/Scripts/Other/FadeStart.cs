1ebcc348f3a26f12b456f95056d8aea6e382d11d
ExcuseMeWhaaaaat <comedyexpert42@gmail.com>
2026-07-12 21:05:39 -0400

    Added Info Panel

--- Assets/Scripts/Other/FadeStart.cs
+++ Assets/Scripts/Other/FadeStart.cs
@@ -4,5 +4,36 @@ using UnityEngine.UI;
 
 public class FadeStart : MonoBehaviour
 {
+    [SerializeField] CanvasGroup canvasGroup;
     
+
+    
+
+    public void StartFadeIn()
+    {
+        StartCoroutine(FadeCoroutine(0));
+        
+    }
+
+    private IEnumerator FadeCoroutine(float startAlpha)
+    {
+        float finalColorAlpha = canvasGroup.alpha;
+        float time = 0;
+        while (finalColorAlpha < 1)
+        {
+            time += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, 1, time);
+            Invoke(nameof(DeleteButton), 1f);
+            yield return null;
+            
+        }
+        
+
+
+    }
+
+    public void DeleteButton()
+    {
+        this.gameObject.SetActive(false);
+    }
 }
--- Assets/Scripts/UI/DelayActivity.cs
+++ Assets/Scripts/UI/DelayActivity.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DelayActivity : MonoBehaviour
 {
-    
+    [SerializeField] Button battleButton;
+    private void Start()
+    {
+        battleButton.gameObject.SetActive(false);
+        Invoke(nameof(DelayButton),3f);
+    }
 
-    
+    public void DelayButton()
+    {
+
+        battleButton.gameObject.SetActive(true);
+    }
 }
